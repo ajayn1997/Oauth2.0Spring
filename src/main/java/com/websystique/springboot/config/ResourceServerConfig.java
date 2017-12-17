0e07f3450869cd6217f61dcfa90040ee303795aa
ajayn1997 <the.ajay.great@gmail.com>
2017-12-17 21:40:59 +0530

    Initial commit

---
+++ src/main/java/com/websystique/springboot/config/ResourceServerConfig.java
@@ -0,0 +1,40 @@
+package com.websystique.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+ 
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+ 
+    private static final String RESOURCE_ID = "my_rest_api";
+     
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID).stateless(false);
+    }
+ 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+        anonymous().disable()
+        .requestMatchers().antMatchers("/user/**")
+        .and().authorizeRequests()
+        .antMatchers("/user/**").access("hasRole('ADMIN')")
+        .antMatchers("/hello").authenticated()
+        .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+ 
+}
